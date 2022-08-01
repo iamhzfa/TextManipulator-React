de24e9c428cd7e0d0be757564b2bd6bc38d9d0d7
iamhzfa <huzefamohammed10@gmail.com>
2022-08-01 20:30:26 +0530

    All Done

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -8,10 +8,11 @@ function Alert(props) {
   }
 
   return (
-    props.alert && <div class={`alert alert-${props.alert.typ} alert-dismissible fade show`} role="alert">
+    <div style={{height:"50px"}}>
+    {props.alert && <div class={`alert alert-${props.alert.typ} alert-dismissible fade show`} role="alert">
       <strong>{capitalize(props.alert.typ)}</strong> : {props.alert.msg}
+    </div>}
     </div>
-    
   )
 }
 
--- src/components/Function.js
+++ src/components/Function.js
@@ -21,6 +21,7 @@ function Function(props) {
         let text = document.getElementById("box");
         text.select();
         navigator.clipboard.writeText(text.value);
+        document.getSelection().removeAllRanges();
         props.showAlert("Copied to Clipboard!", "success");
     }
     const handleRmvSpcClick = () => {
@@ -40,20 +41,20 @@ function Function(props) {
     
         <div className='mx-5 my-3'>
                 <h3 style={{textAlign:"center", color:props.mode==='light' ? '#0d6efd' : 'whitesmoke'}}>Enter the text to analyze</h3>
-            <textarea className="form-control" id="box" style={{backgroundColor:props.mode==='light' ? '#f9f9f9' : '#3e434b8a', color: props.mode==='dark'?'white':'#042743' }} rows="7" value={text} onChange={handleInput} ></textarea>
+            <textarea className="form-control" id="box" style={{backgroundColor:props.mode==='light' ? '#f9f9f9' : '#3e434b8a', color: props.mode==='dark'?'white':'#042743' }} rows="8" value={text} onChange={handleInput} ></textarea>
 
-            <button className={`btn btn-outline-${props.mode==='light'?'primary':'light'} mt-2 btn-box-shadow-none`} onClick={handleUpClick}>Convert to UpperCase</button>
-            <button className={`btn btn-outline-${props.mode==='light'?'primary':'light'} mt-2 mx-2`} onClick={handleLowClick}>Convert to LowerCase</button>
-            <button className={`btn btn-outline-${props.mode==='light'?'primary':'light'} mt-2 mx-2`} onClick={handleCopyClick}>CopyText</button>
-            <button className={`btn btn-outline-${props.mode==='light'?'primary':'light'} mt-2 mx-2`} onClick={handleRmvSpcClick}>Remove ExtraSpaces</button>
-            <button className={`btn btn-${props.mode==='light'?'primary':'light'} mt-2 mx-2`} onClick={handleClearClick}>ClearText</button>
+            <button className={`btn btn-outline-${props.mode==='light'?'primary':'light'} mt-2 btn-box-shadow-none`} onClick={handleUpClick} disabled={text.length===0} >Convert to UpperCase</button>
+            <button className={`btn btn-outline-${props.mode==='light'?'primary':'light'} mt-2 mx-2`} onClick={handleLowClick} disabled={text.length===0} >Convert to LowerCase</button>
+            <button className={`btn btn-outline-${props.mode==='light'?'primary':'light'} mt-2 mx-2`} onClick={handleCopyClick} disabled={text.length===0} >CopyText</button>
+            <button className={`btn btn-outline-${props.mode==='light'?'primary':'light'} mt-2 mx-2`} onClick={handleRmvSpcClick} disabled={text.length===0} >Remove ExtraSpaces</button>
+            <button className={`btn btn-${props.mode==='light'?'primary':'light'} mt-2 mx-2`} onClick={handleClearClick} disabled={text.length===0 } >ClearText</button>
 
         </div>
         <div className="my-5 mx-5 text-center" style={{color:props.mode==='light' ? '#0d6efd' : 'whitesmoke'}}>
         {text.length > 0 ? 
             <>
-            <h4>{text.split(" ").length} words and {text.length} characters</h4>
-            <p><strong>{0.008 * text.split(" ").length}</strong> per minutes required for reading</p>
+            <h4>{text.split(/\s+/).filter((element)=>{return element.length !== 0}).length} words and {text.length} characters</h4>
+            <p><strong>{0.008 * text.split(/\s+/).filter((element)=>{return element.length !== 0}).length}</strong> per minutes required for reading</p>
             <h5>Preview</h5>
             <p>{text}</p>
             </>    
